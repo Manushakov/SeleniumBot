6c53a1d462e372dfc08fea6f1f99a283b1e1a2ad
Rodion <oellekon@mail.ru>
2020-07-18 02:53:20 +0300

    change method of information checking, upgrade main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import telebot
 import constants
+from authorisation import get_marks
 
 bot = telebot.TeleBot(constants.TOKEN_NAME)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=["start", "help"])
 def send_message(message):
-    bot.send_message(message.chat.id, "Бот работает")
+    bot.send_message(message.chat.id, "Добро пожаловать в бота, введите логин и пароль от БРС через пробел, "
+                                      "чтоб узнать свои баллы")
+
+
+@bot.message_handler(content_types="text")
+def send_marks(message):
+    bot.send_message(message.chat.id, "Данные получены, ожидайте ответа")
+    bot.send_message(message.chat.id, get_marks(message.text))
 
 
 bot.polling(none_stop=True)
